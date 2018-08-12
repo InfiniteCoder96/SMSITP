1f38a2aba5b78f9dcd5b72e9e96d4f2411df29fc
Pasindu Vimansa Kulasooriya <pasinduvimansa96@gmail.com>
2018-08-12 10:19:52 +0530

    new admin panel

--- resources/views/Admin/Exam_Centre_Management/viewResults.blade.php
+++ resources/views/Admin/Exam_Centre_Management/viewResults.blade.php
@@ -8,7 +8,51 @@
         <div class="container-fluid">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title m-b-0">Non Academic Staff Details</h5>
+                    <div class="row">
+                        <div class="col-sm-8">
+                            <h5 class="card-title m-b-0">Results Overview</h5>
+                        </div>
+                        <div class="col-sm-2">
+                            <div class="form-group">
+
+
+                                    <select class="select2 form-control custom-select" style="width: 100%; height:36px;" required>
+                                        <option selected disabled>Select a Grade</option>
+
+                                        <option value="WA">Grade 01</option>
+                                        <option value="NV">Grade 01</option>
+                                        <option value="CA">Grade 01</option>
+                                        <option value="WA">Grade 01</option>
+                                        <option value="WA">Grade 01</option>
+                                        <option value="OR">Grade 01</option>
+
+
+                                    </select>
+
+                            </div>
+                        </div>
+                        <div class="col-sm-2">
+                            <div class="form-group">
+
+
+                                <select class="select2 form-control custom-select" style="width: 100%; height:36px;" required>
+                                    <option selected disabled>Select a Class</option>
+
+                                    <option value="WA">A</option>
+                                    <option value="NV">B</option>
+                                    <option value="CA">C</option>
+                                    <option value="WA">D</option>
+                                    <option value="WA">E</option>
+                                    <option value="OR">F</option>
+
+
+                                </select>
+
+                            </div>
+                        </div>
+
+                    </div>
+
                 </div>
                 <div class="table-responsive">
                     <table class="table">
@@ -21,17 +65,15 @@
                                 </label>
                             </th>
                             <th scope="col" style="font-size: 12px">ID</th>
-                            <th scope="col" style="font-size: 12px">DoA</th>
                             <th scope="col" style="font-size: 12px">First Name</th>
                             <th scope="col" style="font-size: 12px">Middle name</th>
                             <th scope="col" style="font-size: 12px">Last Name</th>
-                            <th scope="col" style="font-size: 12px">DoB</th>
-                            <th scope="col" style="font-size: 12px">Gender</th>
-                            <th scope="col" style="font-size: 12px">Address</th>
-                            <th scope="col" style="font-size: 12px">Email</th>
-                            <th scope="col" style="font-size: 12px">Telephone (Res)</th>
-                            <th scope="col" style="font-size: 12px">Telephone (Mob)</th>
-                            <th scope="col" style="font-size: 12px">Status</th>
+                            <th scope="col" style="font-size: 12px">Subjects</th>
+                            <th scope="col" style="font-size: 12px">Total</th>
+                            <th scope="col" style="font-size: 12px">Average</th>
+                            <th scope="col" style="font-size: 12px">Position</th>
+                            <th scope="col" style="font-size: 12px;">Status</th>
+                            <th scope="col" style="font-size: 12px">Remark</th>
                             <th scope="col" style="font-size: 12px">Action</th>
 
                         </tr>
@@ -52,17 +94,15 @@
 
 
                             <td style="font-size: 12px">S123</td>
-                            <td style="font-size: 12px">2018-05-14</td>
                             <td style="font-size: 12px">Gihara</td>
                             <td style="font-size: 12px">Thewara</td>
                             <td style="font-size: 12px">Gihan</td>
-                            <td style="font-size: 12px">1996-11-08</td>
-                            <td style="font-size: 12px">Male</td>
-                            <td style="font-size: 12px">1/A,Poukna Mw,Disa Wawa</td>
-                            <td style="font-size: 12px">parakumbbb@gmail.com</td>
-                            <td style="font-size: 12px">0711254878</td>
-                            <td style="font-size: 12px">0112879456</td>
-                            <td style="font-size: 12px;background-color:red;color: white" >Absent</td>
+                            <td style="font-size: 12px"></td>
+                            <td style="font-size: 12px">360</td>
+                            <td style="font-size: 12px">40.0</td>
+                            <td style="font-size: 12px">40</td>
+                            <td style="font-size: 12px;background-color:red;color: white">Bad</td>
+                            <td style="font-size: 12px;" >Keep it up</td>
                             <td style="font-size: 12px">
                                 <a class="waves-effect waves-dark" href=""><i class="mdi mdi-pencil font-20"></i>
                                     <a class="waves-effect waves-dark" href=""><i class="mdi mdi-delete font-20"></i>
@@ -78,17 +118,15 @@
 
 
                             <td style="font-size: 12px">S123</td>
-                            <td style="font-size: 12px">2018-05-14</td>
                             <td style="font-size: 12px">Gihara</td>
                             <td style="font-size: 12px">Thewara</td>
                             <td style="font-size: 12px">Gihan</td>
-                            <td style="font-size: 12px">1996-11-08</td>
-                            <td style="font-size: 12px">Male</td>
-                            <td style="font-size: 12px">1/A,Poukna Mw,Disa Wawa</td>
-                            <td style="font-size: 12px">parakumbbb@gmail.com</td>
-                            <td style="font-size: 12px">0711254878</td>
-                            <td style="font-size: 12px">0112879456</td>
-                            <td style="font-size: 12px;background-color: green;color: white" >Present</td>
+                            <td style="font-size: 12px"></td>
+                            <td style="font-size: 12px">560</td>
+                            <td style="font-size: 12px">63.2</td>
+                            <td style="font-size: 12px">13</td>
+                            <td style="font-size: 12px;background-color:orange;color: white">Average</td>
+                            <td style="font-size: 12px;" >Keep it up</td>
                             <td style="font-size: 12px">
                                 <a class="waves-effect waves-dark" href=""><i class="mdi mdi-pencil font-20"></i>
                                     <a class="waves-effect waves-dark" href=""><i class="mdi mdi-delete font-20"></i>
